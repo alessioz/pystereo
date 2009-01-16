cf4bb64a5ce9f33d867f1f37aaa1b1162345c48a
alessio <alessio@vivu.(none)>
2009-01-16 17:09:53 +0100

    added correlation

--- pystereo.py
+++ pystereo.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python
-from numarray import *
+from numpy import *
 import Image
 import ImageFilter
 import sys
+import time
+import datetime
+from pylab import *
 
-versione = 0.1
+versione = 0.2
 
-imsx = Image.open("IMG_1376_1.JPG")
-imdx = Image.open("IMG_1377_1.JPG")
+imsx = Image.open("wopmay2sx.jpg")
+imdx = Image.open("wopmay2dx.jpg")
 
 
 #metto in una stringa i valori dei pixel (da Image)
@@ -23,22 +26,29 @@ matricedx = reshape(immaginedx, (imdx.size[1],imdx.size[0]))
 #print type(matricedx)
 
 #dimensioni della finestra scorrevole
-xfinestra = 20
-yfinestra = 20
+xfinestra = 10
+yfinestra = 10
 
 #dimensioni dell'area su cui si effettua l'elaborazione
 xregione = 400
-yregione = 600
+yregione = 500
 
-steprange= 50
+sogliacorr = 1.05
+
+steprange= 70
 #larghezzaimgindx = imdx.size[0]-1
 #lunghezzaimgindx = imdx.size[1]-1
-
 depth=[]
 correlazione=[]
+
+#from scipy.misc.pilutil import *
+numeronull = 0
+numerodati = 0
+inizio = time.clock()
 for riga in range(0,yregione, yfinestra):		# elaborazione riga per riga (in verticale)
 	for finestra in range(0, xregione, xfinestra):
 		indici =[]
+		univoco =[]
 		for step in range(steprange):	# scorrimento della finestra sulla riga
 			somma = 0
 					# qui sotto calcolo la matrice differenza per ogni step
@@ -49,19 +59,56 @@ for riga in range(0,yregione, yfinestra):		# elaborazione riga per riga (in vert
 					# normalizzo la somma e creo una lista con tutti gli indici della riga
 			indice = somma/(xfinestra*yfinestra)
 			indici.append(indice)
-		depth.append(indici.index(min(indici)))  # l'indice col valore pi� passo � quello che ha minore differenza fra le 2 img, ne annoto la posizione (sequenziale per step)
-		#correlazione.append(min(indici))
-	print riga
-	
+		#print mean(indici)/min(indici)
+		depth.append(indici.index(min(indici)))  # l'indice col valore piu basso e' quello che ha minore differenza fra le 2 img, ne annoto la posizione (sequenziale per step)
+		#matricediff = matricedx[riga:riga+yfinestra , finestra:finestra+xfinestra] - matricesx[riga:riga+yfinestra , finestra+indici.index(min(indici)):finestra+xfinestra+indici.index(min(indici))]
+		#nomematricediff = "matrixdiff_row%s_coloumn%s.jpg" % (riga, finestra)
+		#toimage(matricediff).resize((50,50)).save(nomematricediff)
+		univoco = indici
+		minimoassoluto = float(min(univoco))
+		univoco.remove(min(univoco))
+		minimorelativo = float(min(univoco))
+		rapportominimi = minimorelativo/minimoassoluto
+		#print minimorelativo, minimoassoluto, rapportominimi
+		
+		if rapportominimi > sogliacorr:
+			numerodati += 1
+			correlazione.append(1)
+			#plot(indici)
+			#legend()
+			#show()
+			#nomegrafico = "riga%s_finestra%s.png" % (riga,finestra)
+			#savefig(nomegrafico)
+			#close()
+		
+		else:
+			correlazione.append(0)
+			#depth.append(0)
+			#depth.append("None")
+			numeronull +=1
+		#plot(indici)
+		#legend()
+		#show()
+		#nomegrafico = "riga%s_finestra%s.png" % (riga,finestra)
+		#savefig(nomegrafico)
+		#close()
+	stimato = (time.clock()-inizio)/(riga+yfinestra)*(yregione)+inizio
+	print riga, datetime.timedelta(seconds = stimato), datetime.timedelta( seconds = (stimato -time.clock()))
+print numerodati, numeronull
 #print depth
 #print len(depth)
 ydimensione = yregione/yfinestra
 xdimensione = xregione/xfinestra
 
-#correlazioneimage = reshape(correlazione, (ydimensione, xdimensione))
-#print correlazioneimage
+
+	
+correlazioneimage = reshape(correlazione, (ydimensione, xdimensione))
+print correlazioneimage
 depthimage = reshape(depth, (ydimensione, xdimensione))
 print depthimage
+depthimage = correlazioneimage*depthimage
+#from scipy.interpolate.interpolate import interp2d
+#print interp2d(390,500,depthimage)
 
 #if correlazioneimage > 1000:
 	#correlazioneimage = 0
@@ -69,10 +116,15 @@ print depthimage
 
 #vedi masked e spline_filter
 
-nomefile ="IMG_vers%s_xwin%s_ywin%s_step%s.jpg" % (versione, xfinestra, yfinestra, steprange)
+oggetto = "wopmay3"
+dimfiltro = 0
+nomefile ="%s_vers%s_xwin%s_ywin%s_step%s_MedianFilter%s.jpg" % (oggetto,versione, xfinestra, yfinestra, steprange, dimfiltro)
+nomecorrelazione = "correlazione_"+nomefile
 
-from scipy import *
-
-toimage(depthimage).resize((xregione,yregione)).save(nomefile)
-#imsave("correlazione_fungo.jpg", correlazioneimage)
-
+from scipy.misc.pilutil import *
+if dimfiltro == 0:
+	nomefile ="%s_vers%s_xwin%s_ywin%s_step%s_NoFilter_threshold%s.jpg" % (oggetto,versione, xfinestra, yfinestra, steprange, sogliacorr)
+	toimage(depthimage).resize((xregione,yregione)).save(nomefile)
+else:
+	toimage(depthimage).filter(ImageFilter.MedianFilter(dimfiltro)).resize((xregione,yregione)).save(nomefile)
+toimage(correlazioneimage).resize((xregione,yregione)).save(nomecorrelazione)
